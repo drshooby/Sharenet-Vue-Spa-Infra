c0500e5b4fc425a321e8d13944601d643108b781
David Shubov <116600844+shooby-d@users.noreply.github.com>
2025-02-25 13:54:53 -0800

    adding basic ec2 deploy script

---
+++ scripts/ec2/temporary/deploy_tmp_ec2.py
@@ -0,0 +1,31 @@
+import boto3
+import time
+import os
+import sys
+
+# Initialize the boto3 client
+ec2 = boto3.resource('ec2', 
+                    region_name='us-east-1',
+                    aws_access_key_id=os.environ['AWS_ACCESS_KEY'],
+                    aws_secret_access_key=os.environ['AWS_SECRET_KEY'], 
+                    aws_session_token=os.environ['AWS_SESSION'])
+
+# Create the EC2 instance
+instance = ec2.create_instances(
+    ImageId='ami-0c55b159cbfafe1f0',  # Amazon Linux 2 AMI (adjust for your region)
+    InstanceType='t2.micro',
+    MinCount=1,
+    MaxCount=1,
+)[0]
+
+# Wait for the instance to be running
+print(f"Waiting for instance {instance.id} to start...")
+instance.wait_until_running()
+
+# Reload the instance to get the public IP
+instance.reload()
+public_ip = instance.public_ip_address
+
+print(f"Instance is running at {public_ip}")
+
+print(ec2.terminate_instances(InstanceIds=[instance.id]))
