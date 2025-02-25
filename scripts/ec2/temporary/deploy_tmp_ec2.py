8799de4564e5704d2e5fcbc1d49196b2905d1652
David Shubov <116600844+shooby-d@users.noreply.github.com>
2025-02-25 14:49:34 -0800

    cleaned up, was using wrong functions

--- scripts/ec2/temporary/deploy_tmp_ec2.py
+++ scripts/ec2/temporary/deploy_tmp_ec2.py
@@ -2,28 +2,42 @@ import boto3
 import os
 
 # Initialize the boto3 client
-ec2 = boto3.resource('ec2', 
+ec2_client = boto3.client('ec2', 
                     region_name='us-east-1',
                     aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
                     aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'], 
                     aws_session_token=os.environ['AWS_SESSION_TOKEN'])
 
 # Create the EC2 instance
-instance = ec2.create_instances(
+response = ec2_client.run_instances(
     ImageId='ami-04681163a08179f28',  # Amazon Linux 2 AMI (64-bit x86)
     InstanceType='t2.micro',
     MinCount=1,
     MaxCount=1,
-)[0]
+    TagSpecifications=[
+        {
+            'ResourceType': 'instance',
+            'Tags': [
+                {
+                    'Key': 'Name',
+                    'Value': 'Smoke Test'
+                },
+            ]
+        },
+    ]
+)
 
-# Wait for the instance to be running
-print(f"Waiting for instance {instance.id} to start...")
-instance.wait_until_running()
+instance_id = response['Instances'][0]['InstanceId']
 
-# Reload the instance to get the public IP
-instance.reload()
-public_ip = instance.public_ip_address
+print(f"Waiting for instance {instance_id} to start...")
+waiter = ec2_client.get_waiter('instance_running')
+waiter.wait(InstanceIds=[instance_id])
+
+describe_response = ec2_client.describe_instances(InstanceIds=[instance_id])
+public_ip = describe_response['Reservations'][0]['Instances'][0].get('PublicIpAddress', 'No public IP')
 
 print(f"Instance is running at {public_ip}")
 
-print(ec2.terminate_instances(InstanceIds=[instance.id]))
+# Kill the instance
+terminate_response = ec2_client.terminate_instances(InstanceIds=[instance_id])
+print(terminate_response)
