cf1d403cde1756bd60da2377a7639720ad4556fb
David Shubov <116600844+drshooby@users.noreply.github.com>
2025-03-01 14:36:18 -0800

    adding latest script version, manually messing with the ec2 to finalize things

--- scripts/ec2/use_latest.py
+++ scripts/ec2/use_latest.py
@@ -9,22 +9,23 @@ try:
 
     AWS_ACCOUNT_ID = sts_client.get_caller_identity()["Account"]
 
-    ECR_REPO_FRONTEND = f"{AWS_ACCOUNT_ID}.dkr.ecr.{AWS_REGION}.amazonaws.com/sharenet_vue_spa_frontend:latest"
     ECR_REPO_BACKEND = f"{AWS_ACCOUNT_ID}.dkr.ecr.{AWS_REGION}.amazonaws.com/sharenet_vue_spa_backend:latest"
+    ECR_REPO_FRONTEND = f"{AWS_ACCOUNT_ID}.dkr.ecr.{AWS_REGION}.amazonaws.com/sharenet_vue_spa_frontend:latest"
 
     commands = [
         f"aws ecr get-login-password --region {AWS_REGION} | docker login --username AWS --password-stdin {AWS_ACCOUNT_ID}.dkr.ecr.{AWS_REGION}.amazonaws.com",
 
         # Pull the latest
-        f"docker pull {ECR_REPO_FRONTEND}",
         f"docker pull {ECR_REPO_BACKEND}",
+        f"docker pull {ECR_REPO_FRONTEND}",
 
-        # Kill old, bring up new
+        # Kill old
         "docker stop frontend backend || true",
         "docker rm -f frontend backend || true",
 
-        f"docker run -d --name frontend -p 8080:8080 {ECR_REPO_FRONTEND}",
+        # Bring up new
         f"docker run -d --name backend -p 5000:5000 {ECR_REPO_BACKEND}",
+        f"docker run -d --name frontend -p 8080:8080 {ECR_REPO_FRONTEND}",
 
         "docker ps -a"
     ]
